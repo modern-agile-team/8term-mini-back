8c13e92235dd8d4c1dbb53dc28cd14afaeb0b5cb
JeonHeena <heena0727@gmail.com>
2024-08-06 16:59:47 +0900

    로그인, 회원가입 get, post요청에 따른 경로 설정했습니다.

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -4,7 +4,14 @@ const express = require("express");
 const router = express.Router();
 
 const newctrl = require("../movieInfo/movieInfo.ctrl");
+const userCtrl = require("../user/user.ctrl");
 
 router.get("/", newctrl.process.check);
+
+router.get("/login", userCtrl.output.login);
+router.get("/register", userCtrl.output.register);
+
+router.post("/login", userCtrl.process.login);
+router.post("/register", userCtrl.process.register);
 
 module.exports = router;
