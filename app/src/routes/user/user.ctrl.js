9133896e58e83d36ec2c174b6ecd839e75046c43
JeonHeena <heena0727@gmail.com>
2024-08-06 17:03:17 +0900

    로그인과 회원가입 요청 처리와 그에 따른 HTTP 상태코드와 응답을 작성했습니다

--- app/src/routes/user/user.ctrl.js
+++ app/src/routes/user/user.ctrl.js
@@ -1,1 +1,44 @@
 "use strict";
+
+const userService = require("../../services/userService");
+
+const output = {
+  login: (req, res) => {
+    res.render("user/login");
+  },
+
+  register: (req, res) => {
+    res.render("user/register");
+  },
+};
+
+const process = {
+  login: async (req, res) => {
+    const userservice = new userService(req.body);
+    const response = await userservice.login();
+
+    const url = {
+      method: "POST",
+      path: "/login",
+      status: response.err ? 400 : 200,
+    };
+    return res.status(url.status).json(response);
+  },
+
+  register: async (req, res) => {
+    const userservice = new userService(req.body);
+    const response = await userservice.register();
+
+    const url = {
+      method: "POST",
+      path: "/register",
+      status: response.err ? 401 : 201,
+    };
+    return res.status(url.status).json(response);
+  },
+};
+
+module.exports = {
+  output,
+  process,
+};
