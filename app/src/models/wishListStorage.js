548b4d92a372256bec42c3f1689bf81bbf908c45
dg1418 <dg1418@naver.com>
2024-08-07 20:23:47 +0900

    feature/찜 레퍼리토리 API/#23

---
+++ app/src/models/wishListStorage.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const db = require("../config/db");
+
+class WishListStorage {
+  static getWishListInfo(id) {
+    const query = "SELECT * FROM wish_list WHERE wish_list_id = ?";
+    return db.query(query, [id]);
+  }
+
+  static getWishListInfoAll() {
+    const query = "SELECT * FROM wish_list";
+    return db.query(query);
+  }
+
+  static addWishList(userId, movieId) {
+    const query = "INSERT INTO wish_list (user_id, movie_id) VALUES (?, ?)";
+    return db.query(query, [userId, movieId]);
+  }
+
+  static deleteWishList(id) {
+    const query = "DELETE FROM wish_list WHERE wish_list_id = ?";
+    return db.query(query, [id]);
+  }
+}
+
+module.exports = WishListStorage;
