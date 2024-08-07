b2a2fa3141426b265f9fe0e39eca30b99c2444f5
JeonHeena <heena0727@gmail.com>
2024-08-07 17:35:25 +0900

    userStorage insert문 쿼리 작성했습니다.

--- app/src/models/userStorage.js
+++ app/src/models/userStorage.js
@@ -1,1 +1,16 @@
 "use strict";
+
+import db from "../config/db";
+
+class UserStorage {
+  static postUserInfo(userInfo) {
+    const query = "INSERT INTO user(id, password, nickname) VALUES(?,?,?);";
+    const id = userInfo.id;
+    const password = userInfo.password;
+    const nickname = userInfo.nickname;
+
+    return db.query(query, [id, password, nickname]);
+  }
+}
+
+export default UserStorage;
