916369b59a9fcfd4937cbb7465c1144e4416ed90
JeonHeena <heena0727@gmail.com>
2024-08-07 17:37:17 +0900

    userService, 컨트롤러 내용추가와 수정사항입니다.

--- app/src/routes/user/user.ctrl.js
+++ app/src/routes/user/user.ctrl.js
@@ -1,44 +1,14 @@
 "use strict";
 
-const userService = require("../../services/userService");
-
-const output = {
-  login: (req, res) => {
-    res.render("user/login");
-  },
-
-  register: (req, res) => {
-    res.render("user/register");
-  },
-};
+import UserService from "../../services/userService";
 
 const process = {
-  login: async (req, res) => {
-    const userservice = new userService(req.body);
-    const response = await userservice.login();
+  signUp: async (req, res) => {
+    const userService = new UserService(req);
+    const response = await userService.signUp();
 
-    const url = {
-      method: "POST",
-      path: "/login",
-      status: response.err ? 400 : 200,
-    };
-    return res.status(url.status).json(response);
-  },
-
-  register: async (req, res) => {
-    const userservice = new userService(req.body);
-    const response = await userservice.register();
-
-    const url = {
-      method: "POST",
-      path: "/register",
-      status: response.err ? 401 : 201,
-    };
-    return res.status(url.status).json(response);
+    return res.json(response);
   },
 };
 
-module.exports = {
-  output,
-  process,
-};
+export default signUp;
