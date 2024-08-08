d9100a4ba547168242db48fd66f4471c523e6130
JeonHeena <heena0727@gmail.com>
2024-08-08 11:15:13 +0900

    라우팅설정/#24

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -4,8 +4,11 @@ const express = require("express");
 const router = express.Router();
 
 const movieInfoCtrl = require("../movieInfo/movieInfo.ctrl");
+const userCtrl = require("../user/user.ctrl");
 
 router.get("/movies", movieInfoCtrl.process.getMovie);
 router.get("/movies/:id", movieInfoCtrl.process.getMovie);
+
+router.post("/users", userCtrl.process.signUp);
 
 module.exports = router;
