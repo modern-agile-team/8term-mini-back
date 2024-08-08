a60188f31e2509ba6d0296405af19647a8f914ac
gwgw123 <yja20850@naver.com>
2024-08-08 20:21:18 +0900

    setting/index 라우팅 주석 추가/#26

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -3,19 +3,21 @@
 const express = require("express");
 const router = express.Router();
 
+// 컨트롤러
 const movieInfoCtrl = require("../movieInfo/movieInfo.ctrl");
 const reviewInfoCtrl = require("../reviewInfo/reviewInfo.ctrl");
 const commentInfoCtrl = require("../commentInfo/commentInfo.ctrl");
 
+// movie 라우팅
 router.get("/movies", movieInfoCtrl.process.getMovie);
 router.get("/movies/:id", movieInfoCtrl.process.getMovie);
 
-//review 라우터
+// review 라우팅
 router.get("/movies/:id/reviews", reviewInfoCtrl.process.getReview); // 뒤에 페이지 쿼리 추가해야 됨
 router.post("/movies/:id/reviews", reviewInfoCtrl.process.addReview);
 router.delete("/users/my/reviews/:id", reviewInfoCtrl.process.removeReview);
 
-//comment 라우터
+// comment 라우팅
 router.get("/reviews/:id/comments", commentInfoCtrl.process.getComment); // 뒤에 페이지 쿼리 추가해야 됨
 router.post("/reviews/:id/comments", commentInfoCtrl.process.addComment);
 router.delete("/users/my/comments/:id", commentInfoCtrl.process.removeComment);
