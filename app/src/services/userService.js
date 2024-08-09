a512c9d66f2e7ffb5b8a210b5c04f5717516bb07
JeonHeena <heena0727@gmail.com>
2024-08-09 15:14:43 +0900

    회원가입 비밀번호 해싱작업(+bcrypt설치)/#24

--- app/src/services/userService.js
+++ app/src/services/userService.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const bcrypt = require("bcrypt"); //bcrypt라이브러리 이용하여 비밀번호 해싱
 const UserStorage = require("../models/userStorage");
 
 class UserService {
@@ -10,15 +11,19 @@ class UserService {
   async signUp() {
     const userInfo = this.body;
 
-    //비밀번호확인
+    //비밀번호입력값과 비밀번호 확인 입력값이 다른 경우(오류처리)
     if (userInfo.password !== userInfo.confirmPassword) {
       return { success: false, msg: "비밀번호가 다릅니다." };
     }
-    //
+    //비밀번호 해싱
+    const saltRounds = 10; //솔트 라운드 수 (10으로 설정)
+    const hashedPassword = await bcrypt.hash(userInfo.password, saltRounds);
+
+    //db에 저장
     const response = await UserStorage.postUserInfo({
       nickname: userInfo.nickname,
       id: userInfo.id,
-      password: userInfo.password,
+      password: hashedPassword,
     });
     return response;
   }
