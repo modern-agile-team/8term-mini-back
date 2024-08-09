786e6f485d1385cd9dfed309571e34d8bc2ff115
JeonHeena <heena0727@gmail.com>
2024-08-09 13:41:11 +0900

    비밀번호값, 비밀번호 확인값 같은지 확인하는 if문 작성/#24

--- app/src/services/userService.js
+++ app/src/services/userService.js
@@ -9,6 +9,12 @@ class UserService {
 
   async signUp() {
     const userInfo = this.body;
+
+    //비밀번호확인
+    if (userInfo.password !== userInfo.confirmPassword) {
+      return { success: false, msg: "비밀번호가 다릅니다." };
+    }
+    //
     const response = await UserStorage.postUserInfo({
       nickname: userInfo.nickname,
       id: userInfo.id,
