7385a4f417d23b05bc8e8a61222a7547cf4ca041
JeonHeena <heena0727@gmail.com>
2024-08-12 15:12:19 +0900

    회원가입 기능 해싱 수정/#24

--- app/src/models/userStorage.js
+++ app/src/models/userStorage.js
@@ -3,7 +3,7 @@
 const db = require("../config/db");
 
 class UserStorage {
-  static postUserInfo({ nickname, id, password }) {
+  static addUserInfo(nickname, id, password) {
     const query = "INSERT INTO user(nickname, id, password) VALUES(?,?,?);";
     return db.query(query, [nickname, id, password]);
   }
--- app/src/services/userService.js
+++ app/src/services/userService.js
@@ -13,19 +13,19 @@ class UserService {
 
     //비밀번호입력값과 비밀번호 확인 입력값이 다른 경우(오류처리)
     if (userInfo.password !== userInfo.confirmPassword) {
-      return { success: false, msg: "비밀번호가 다릅니다." };
+      return { success: false, msg: "비밀번호가 다릅니다." }; //status코드값으로 적기
     }
     //비밀번호 해싱
     const saltRounds = 10; //솔트 라운드 수 (10으로 설정)
     const hashedPassword = await bcrypt.hash(userInfo.password, saltRounds);
 
     //db에 저장
-    const response = await UserStorage.postUserInfo({
-      nickname: userInfo.nickname,
-      id: userInfo.id,
-      password: hashedPassword,
-    });
-    return response;
+    const response = await UserStorage.addUserInfo(
+      userInfo.nickname,
+      userInfo.id,
+      hashedPassword
+    );
+    return response; //status코드값으로 적기
   }
 }
 
