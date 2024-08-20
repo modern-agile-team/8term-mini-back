85c49082552859eb0af589508c17d28e6a0743ac
dg1418 <dg1418@naver.com>
2024-08-20 14:50:35 +0900

    setting/cors적용/#48

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@
 //모듈
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 dotenv.config();
 
 const app = express();
@@ -10,6 +11,9 @@ const home = require("./src/routes/home");
 
 //미들웨어
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 //앱 세팅
 app.set("views", "./src/views");
