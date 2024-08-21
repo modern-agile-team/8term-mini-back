e95ebbde0a26065e8a8c8ad48f111d4a5604ae04
dg1418 <dg1418@naver.com>
2024-08-21 14:30:18 +0900

    modify/res값 변경사항

--- app/src/routes/movieInfo/movieInfo.ctrl.js
+++ app/src/routes/movieInfo/movieInfo.ctrl.js
@@ -10,8 +10,8 @@ const process = {
   },
   getMovie: async (req, res) => {
     const movie = new Movie(req);
-    const response = await movie.getMovie();
-    return res.status(response.status).json(response);
+    const { status, data } = await movie.getMovie();
+    return res.status(status).json(data);
   },
 };
 
