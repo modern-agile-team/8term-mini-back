0539dd9b6824f7bd2da91b055f67a5325af9f73f
dg1418 <dg1418@naver.com>
2024-08-21 15:28:20 +0900

    modify/movie,wish-list관련 리팩토링/#57

--- app/src/services/movieService.js
+++ app/src/services/movieService.js
@@ -31,23 +31,7 @@ class MovieService {
       }
       return { status: 200, data: response[0] };
     } catch (error) {
-      // testfunc(error.code) 하면  =>   //obj가  => { status: 503, data: { error: "서버 오류" } 이런 오브젝트를 리턴하도록
-      console.error("오류 메시지:", error.message);
-      console.error("오류 코드:", error.code);
-
-      switch (error.code) {
-        case "ECONNREFUSED":
-          return { status: 503, data: { error: "서버 오류" } };
-        case "ER_PARSE_ERROR":
-          return { status: 500, data: { error: "서버 오류" } };
-        case "ETIMEOUT":
-          return {
-            status: 504,
-            data: { error: "서버 오류" },
-          };
-        default:
-          return { status: 500, data: { error: "서버 오류" } };
-      }
+      return { status: 500, data: { error: "서버 오류" } };
     }
   }
 
@@ -57,22 +41,7 @@ class MovieService {
       const response = await MovieStorage.getMovieInfo(id);
       return { status: 200, data: response[0] };
     } catch (error) {
-      console.error("오류 메시지:", error.message);
-      console.error("오류 코드:", error.code);
-
-      switch (error.code) {
-        case "ECONNREFUSED":
-          return { status: 503, data: { error: "서버 오류" } };
-        case "ER_PARSE_ERROR":
-          return { status: 500, data: { error: "서버 오류" } };
-        case "ETIMEOUT":
-          return {
-            status: 504,
-            data: { error: "서버 오류" },
-          };
-        default:
-          return { status: 500, data: { error: "서버 오류" } };
-      }
+      return { status: 500, data: { error: "서버 오류" } };
     }
   }
 }
--- app/src/services/wishListService.js
+++ app/src/services/wishListService.js
@@ -14,22 +14,7 @@ class WishListService {
       const response = await WishListStorage.getUserWishListInfo(userId);
       return { status: 200, data: response[0] };
     } catch (error) {
-      console.error("오류 메시지:", error.message);
-      console.error("오류 코드:", error.code);
-
-      switch (error.code) {
-        case "ECONNREFUSED":
-          return { status: 503, data: { error: "서버 오류" } };
-        case "ER_PARSE_ERROR":
-          return { status: 500, data: { error: "서버 오류" } };
-        case "ETIMEOUT":
-          return {
-            status: 504,
-            data: { error: "서버 오류" },
-          };
-        default:
-          return { status: 500, data: { error: "서버 오류" } };
-      }
+      return { status: 500, data: { error: "서버 오류" } };
     }
   }
 
@@ -40,29 +25,14 @@ class WishListService {
       const check = await WishListStorage.getWishListInfo(userId, movieId);
       if (!check[0]) {
         //400 -> 409
-        return { status: 400, data: { error: "이미 찜이 생성됨" } };
+        return { status: 409, data: { error: "이미 찜이 생성됨" } };
       }
       const wishListId = (await WishListStorage.addWishListInfo(userId, movieId))[0].insertId;
 
       const response = await WishListStorage.processWishListInfo(wishListId);
-      return { status: 200, data: response[0] };
+      return { status: 201, data: response[0] };
     } catch (error) {
-      console.error("오류 메시지:", error.message);
-      console.error("오류 코드:", error.code);
-
-      switch (error.code) {
-        case "ECONNREFUSED":
-          return { status: 503, data: { error: "서버 오류" } };
-        case "ER_PARSE_ERROR":
-          return { status: 500, data: { error: "서버 오류" } };
-        case "ETIMEOUT":
-          return {
-            status: 504,
-            data: { error: "서버 오류" },
-          };
-        default:
-          return { status: 500, data: { error: "서버 오류" } };
-      }
+      return { status: 500, data: { error: "서버 오류" } };
     }
   }
 
@@ -72,25 +42,10 @@ class WishListService {
       const check = (await WishListStorage.removeWishListInfo(wishListId))[0].affectedRows;
       return check
         ? // 200 -> 204
-          { status: 200 }
+          { status: 204 }
         : { status: 400, data: { error: "지워진 값이 없습니다." } };
     } catch (error) {
-      console.error("오류 메시지:", error.message);
-      console.error("오류 코드:", error.code);
-
-      switch (error.code) {
-        case "ECONNREFUSED":
-          return { status: 503, data: { error: "서버 오류" } };
-        case "ER_PARSE_ERROR":
-          return { status: 500, data: { error: "서버 오류" } };
-        case "ETIMEOUT":
-          return {
-            status: 504,
-            data: { error: "서버 오류" },
-          };
-        default:
-          return { status: 500, data: { error: "서버 오류" } };
-      }
+      return { status: 500, data: { error: "서버 오류" } };
     }
   }
 }
