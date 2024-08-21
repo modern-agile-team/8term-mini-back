ee89f08655967dbf84090c3d20cf248c90f829be
JeonHeena <heena0727@gmail.com>
2024-08-21 13:42:15 +0900

    modify/회원가입수정/#55

--- app/src/models/userStorage.js
+++ app/src/models/userStorage.js
@@ -12,6 +12,7 @@ class UserStorage {
     const query = "SELECT * FROM user WHERE user_id =?;";
     return db.query(query, [userId]);
   }
+
   static getUserInfo(id) {
     const query = "SELECT * FROM user WHERE id = ?;";
     return db.query(
