380c75061e95acdd66b66b4ca125d6bac5537f09
dg1418 <dg1418@naver.com>
2024-08-28 11:48:57 +0900

    modify/app.js 순서다시원상태/88

--- app/app.js
+++ app/app.js
@@ -2,19 +2,23 @@
 
 //모듈
 const express = require("express");
-const cors = require("cors");
 const dotenv = require("dotenv");
-const home = require("./src/routes/home");
+const cors = require("cors");
+dotenv.config();
 
 const app = express();
-dotenv.config();
+const home = require("./src/routes/home");
 
 //미들웨어
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://127.0.0.1:3000", "https://mordern-movie.netlify.app"],
+    origin: [
+      "http://localhost:3000",
+      "http://127.0.0.1:3000",
+      "https://mordern-movie.netlify.app/",
+    ],
     credentials: true,
   })
 );
