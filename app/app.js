5fd77715cb335a323dcab7ab06cc6f247f3fd564
dg1418 <dg1418@naver.com>
2024-08-28 10:56:57 +0900

    modify/app.js cors옵션추가/#88

--- app/app.js
+++ app/app.js
@@ -2,27 +2,24 @@
 
 //모듈
 const express = require("express");
-const dotenv = require("dotenv");
 const cors = require("cors");
-dotenv.config();
-
-const app = express();
+const dotenv = require("dotenv");
 const home = require("./src/routes/home");
 
-const corsOptions = {
-  origin: ["http://localhost:3000", "http://127.0.0.1:3000"], // 요청을 허용할 도메인
-  credentials: true, // 자격 증명을 허용
-};
+const app = express();
+dotenv.config();
 
 //미들웨어
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://127.0.0.1:3000", "https://mordern-movie.netlify.app"],
+    credentials: true,
+  })
+);
 
-app.use(cors(corsOptions));
-
-//앱 세팅
-app.set("views", "./src/views");
-
+//라우팅
 app.use("/", home);
 
 module.exports = app;
