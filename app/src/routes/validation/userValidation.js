6a3b2c1cad9a2ee5ab9bba9fc918ddf0aa3797c6
이건우 <yja20850@naver.com>
2024-08-28 13:48:56 +0900

    feature/user validation 구현, 적용 전/#75

--- app/src/routes/validation/userValidation.js
+++ app/src/routes/validation/userValidation.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const { body, validationResult } = require("express-validator");
+const { body, param, validationResult } = require("express-validator");
 
 const checkAddUser = [
   body("nickname")
@@ -64,5 +64,74 @@ const checkUser = [
     return res.status(400).json({ data: errors.array()[0].msg });
   },
 ];
+
+// const checkUserId = [
+//   body("id").exists().withMessage("id 전달 오류").bail(),
+
+//   (req, res, next) => {
+//     const errors = validationResult(req);
+
+//     if (errors.isEmpty()) {
+//       return next();
+//     }
+
+//     return res.status(400).json({ data: errors.array()[0].msg });
+//   },
+// ];
+
+// const checkUpdateUser = [
+//   param("user_id")
+//     .exists()
+//     .withMessage("user_id 전달 오류")
+//     .bail()
+//     .isInt()
+//     .withMessage("id 입력 오류")
+//     .bail(),
+
+//   body("nickname")
+//     .exists()
+//     .withMessage("nickname 전달 오류")
+//     .bail()
+//     .matches(/^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,10}$/)
+//     .withMessage("nickname 입력 오류")
+//     .bail(),
+
+//   body("password")
+//     .exists()
+//     .withMessage("password 전달 오류")
+//     .bail()
+//     .matches(/^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9!@#$%^&*()._-]{6,16}$/)
+//     .withMessage("password 입력 오류")
+//     .bail(),
+
+//   body("confirmPassword")
+//     .exists()
+//     .withMessage("confirmPassword 전달 오류")
+//     .bail()
+//     .custom((value, { req }) => {
+//       if (value !== req.body.password) {
+//         throw new Error("confirmPassword 입력 오류");
+//       }
+//       return true;
+//     }),
+
+//   body("profile")
+//     .exists()
+//     .withMessage("profile 전달 오류")
+//     .bail()
+//     .matches(/^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9!@#$%^&*()._-]{6,16}$/)
+//     .withMessage("profile 입력 오류")
+//     .bail(),
+
+//   (req, res, next) => {
+//     const errors = validationResult(req);
+
+//     if (errors.isEmpty()) {
+//       return next();
+//     }
+
+//     return res.status(400).json({ data: errors.array()[0].msg });
+//   },
+// ];
 
 module.exports = { checkAddUser, checkUser };
